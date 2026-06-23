1b20567798e3d8d5ddbaacd213a03fa3040c86ea
Sami Bosnjak <sami.bosnjak@hva.nl>
2026-06-24 00:22:58 +0200

    feat: movie suggestions will now be added to the poll.

--- commands/utilities/PollCommandUtility.js
+++ commands/utilities/PollCommandUtility.js
@@ -1,17 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { moviesDataChannelID } = require('../../config.json');
+const DISCORD_POLL_LIMIT = 10;
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('poll').setDescription('Creates a poll deciding what movies to watch this week'),
+	data: new SlashCommandBuilder().setName('poll').setDescription('Creates a poll deciding what movies to watch this week').addIntegerOption((option) =>
+		option.setName('hours')
+			.setDescription('How many hours the poll stays open (default 24)')
+			.setMinValue(1)
+			.setMaxValue(768)),
 	async execute(interaction) {
+		const hours = interaction.options.getInteger('hours') ?? 24;
+		const channel = await interaction.client.channels.fetch(moviesDataChannelID);
+		const messages = await channel.messages.fetch({ limit: DISCORD_POLL_LIMIT });
+
+		const titles = messages.map((message) => {
+			const data = JSON.parse(message.content);
+			return data.title;
+		});
+
+        		const answers = titles.map((title) => ({ text: title }));
+
 		await interaction.reply({
 			poll: {
 				question: { text: 'What should we watch this week?' },
-				answers: [
-					{ text: 'Interstellar' },
-					{ text: 'Parasite' },
-					{ text: 'The Matrix' },
-				],
-				duration: 1,
+				answers,
+				duration: hours,
 				allowMultiselect: false,
 			},
 		});
